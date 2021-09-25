ebeb2901e893260aa622c56027c5c0f7d52a1854
Dan <daniel.klokov@mhub.tv>
2021-09-25 21:37:00 +0100

    Feature/create user location db context (#4)
    * Registered UserLocationsContext data base context
    
    * UserLocationContext loaded with mock data
    
    * New UsersController to get user mock data

--- LocationRESTAPI/Startup.cs
+++ LocationRESTAPI/Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using LocationRESTAPI.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,6 +29,9 @@ namespace LocationRESTAPI
         {
 
             services.AddControllers();
+
+            // Registered DB contexts
+            services.AddDbContext<UserLocationContext>(options => options.UseInMemoryDatabase("UserLocations"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -47,6 +52,39 @@ namespace LocationRESTAPI
             {
                 endpoints.MapControllers();
             });
+
+            // Populate in memory DB with mock data
+            var scope = app.ApplicationServices.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<UserLocationContext>();
+            LoadMockData(context);
+        }
+
+        private static void LoadMockData(UserLocationContext context)
+        {
+            var users = new List<User>
+            {
+                new User
+                {
+                    Id = Guid.Parse("f53ed0af-cc06-4120-bce4-97ce4f4a00e5"),
+                    FullName = "John Johnson",
+                    Email = "john.johnson@locations.co.uk"
+                },
+                new User
+                {
+                    Id = Guid.Parse("e3ce686a-ff0b-4eac-a931-693106a257c6"),
+                    FullName = "Mark Smith",
+                    Email = "mark.smith@locations.co.uk"
+                },
+                new User
+                {
+                    Id = Guid.Parse("63677671-3488-45e0-bbe6-92c9d1d8f965"),
+                    FullName = "Jake Jackson",
+                    Email = "jake.jackson@locations.co.uk"
+                },
+            };
+
+            context.AddRange(users.ToArray());
+            context.SaveChanges();
         }
     }
 }
