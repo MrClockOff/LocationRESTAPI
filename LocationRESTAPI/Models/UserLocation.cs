b3f8b2ebd32215ae69e09ec14900a6229bff7dec
Daniel Klokov <daniel.klokov@mhub.tv>
2021-09-25 19:58:28 +0100

    New UserLocation model

---
+++ LocationRESTAPI/Models/UserLocation.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace LocationRESTAPI.Models
+{
+    /// <summary>
+    /// User location model which represent user's logged location and time
+    /// </summary>
+    public class UserLocation
+    {
+        /// <summary>
+        /// Model constructor
+        /// </summary>
+        public UserLocation()
+        {
+        }
+
+        /// <summary>
+        /// Location entry unique Id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Location latitude in degrees
+        /// </summary>
+        public double Latitude { get; set; }
+
+        /// <summary>
+        /// Location longitude in degrees
+        /// </summary>
+        public double Longitude { get; set; }
+
+        /// <summary>
+        /// Entry date and time
+        /// </summary>
+        public DateTime DateTime { get; set; }
+    }
+}
