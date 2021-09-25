990d918339a6832beb415c17611e2ae88c0c4087
Daniel Klokov <daniel.klokov@mhub.tv>
2021-09-25 22:39:59 +0100

    Request handler for posting user location

--- LocationRESTAPI/Controllers/UserLocationController.cs
+++ LocationRESTAPI/Controllers/UserLocationController.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Threading.Tasks;
+using LocationRESTAPI.Models;
+using LocationRESTAPI.Models.DataTransferObjects;
+using LocationRESTAPI.Models.Extensions;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace LocationRESTAPI.Controllers
@@ -11,14 +16,52 @@ namespace LocationRESTAPI.Controllers
     public class UserLocationController : ControllerBase
     {
         private readonly ILogger<UserLocationController> _logger;
+        private readonly UserLocationContext _userLocationContext;
 
         /// <summary>
-        /// Controller constructor with injected logger
+        /// Controller constructor with injected ILogger and UserLocationContext
         /// </summary>
         /// <param name="logger"></param>
-        public UserLocationController(ILogger<UserLocationController> logger)
+        /// <param name="userLocationContext"></param>
+        public UserLocationController(
+            ILogger<UserLocationController> logger,
+            UserLocationContext userLocationContext)
         {
             _logger = logger;
+            _userLocationContext = userLocationContext;
         }
+
+        /// <summary>
+        /// Post user location
+        /// </summary>
+        /// <param name="userId"></param>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        [HttpPost("users/{userId}")]
+        public async Task<ActionResult<UserLocationDTO>> PostUserLocation(Guid userId, [FromBody] LocationDTO location)
+        {
+            // Check first that user does exist
+            var user = await _userLocationContext.Users.FindAsync(userId);
+
+            if (user == null)
+            {
+                return NotFound($"User not found (Id: {userId})");
+            }
+
+            // Add user location
+            var userLocation = new UserLocation
+            {
+                Latitude = location.Latitude,
+                Longitude = location.Longitude,
+                DateTime = DateTime.UtcNow
+            };
+
+            _userLocationContext.UserLocations.Add(userLocation);
+            user.Locations.Add(userLocation);
+            await _userLocationContext.SaveChangesAsync();
+
+            var result = userLocation.ToDTO();
+            return result;
+        }            
     }
 }
---
+++ LocationRESTAPI/Models/Extensions/UserLocationExtensions.cs
@@ -0,0 +1,28 @@
+﻿using LocationRESTAPI.Models.DataTransferObjects;
+
+namespace LocationRESTAPI.Models.Extensions
+{
+    /// <summary>
+    /// UserLocation model extensions
+    /// </summary>
+    public static class UserLocationExtensions
+    {
+        /// <summary>
+        /// Convert UserLocation model into DTO
+        /// </summary>
+        /// <param name="location"></param>
+        /// <returns></returns>
+        public static UserLocationDTO ToDTO(this UserLocation location)
+        {
+            var dto = new UserLocationDTO
+            {
+                Latitude = location.Latitude,
+                Longitude = location.Longitude,
+                UserId = location.User.Id,
+                DateTime = location.DateTime
+            };
+
+            return dto;
+        }
+    }
+}
