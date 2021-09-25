04c971a5b509c2c54f34088d9cd1e0f2f2e4b2dc
Daniel Klokov <daniel.klokov@mhub.tv>
2021-09-25 20:45:06 +0100

    UserLocationContext loaded with mock data

--- LocationRESTAPI/Startup.cs
+++ LocationRESTAPI/Startup.cs
@@ -52,6 +52,39 @@ namespace LocationRESTAPI
             {
                 endpoints.MapControllers();
             });
+
+            // Populate in memory DB with mock data
+            var scope = app.ApplicationServices.CreateScope();
+            var context = scope.ServiceProvider.GetRequiredService<UserLocationContext>();
+            LoadMockData(context);
+        }
+
+        private static void LoadMockData(UserLocationContext context)
+        {
+            var users = new List<User>
+            {
+                new User
+                {
+                    Id = Guid.Parse("f53ed0af-cc06-4120-bce4-97ce4f4a00e5"),
+                    FullName = "John Johnson",
+                    Email = "john.johnson@locations.co.uk"
+                },
+                new User
+                {
+                    Id = Guid.Parse("e3ce686a-ff0b-4eac-a931-693106a257c6"),
+                    FullName = "Mark Smith",
+                    Email = "mark.smith@locations.co.uk"
+                },
+                new User
+                {
+                    Id = Guid.Parse("63677671-3488-45e0-bbe6-92c9d1d8f965"),
+                    FullName = "Jake Jackson",
+                    Email = "jake.jackson@locations.co.uk"
+                },
+            };
+
+            context.AddRange(users.ToArray());
+            context.SaveChanges();
         }
     }
 }
