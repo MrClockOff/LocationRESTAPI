a42ca7d81c106c789a566a25e45a30abaf63ab72
Daniel Klokov <daniel.klokov@mhub.tv>
2021-09-25 20:11:49 +0100

    New UserLocationController

---
+++ LocationRESTAPI/Controllers/UserLocationController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace LocationRESTAPI.Controllers
+{
+    /// <summary>
+    /// User location controller for handling user location logging and retrieval
+    /// </summary>
+    [ApiController]
+    [Route("api/location")]
+    public class UserLocationController : ControllerBase
+    {
+        private readonly ILogger<UserLocationController> _logger;
+
+        /// <summary>
+        /// Controller constructor with injected logger
+        /// </summary>
+        /// <param name="logger"></param>
+        public UserLocationController(ILogger<UserLocationController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
