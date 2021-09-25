c07d87d69c756574f8efc0d3702ab1a3a7a30891
Dan <daniel.klokov@mhub.tv>
2021-09-25 20:06:18 +0100

    Feature/create user location and user entities (#2)
    * New User model
    
    * New UserLocation model
    
    * User and UserLocation relationship

---
+++ LocationRESTAPI/Models/User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LocationRESTAPI.Models
+{
+    /// <summary>
+    /// User model
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Model constructor
+        /// </summary>
+        public User()
+        {
+        }
+
+        /// <summary>
+        /// User's unique Id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// User's full name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// User's unique email address
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// User's related locations
+        /// </summary>
+        public List<UserLocation> Locations { get; set; }
+    }
+}
