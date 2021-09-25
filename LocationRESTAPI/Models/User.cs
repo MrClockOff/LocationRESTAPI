e9f443c5e882fe4b3ae41f14af3b495d4e7b8714
Daniel Klokov <daniel.klokov@mhub.tv>
2021-09-25 19:52:43 +0100

    New User model

---
+++ LocationRESTAPI/Models/User.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace LocationRESTAPI.Models
+{
+    /// <summary>
+    /// User model
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Model constructor
+        /// </summary>
+        public User()
+        {
+        }
+
+        /// <summary>
+        /// User's unique Id
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// User's full name
+        /// </summary>
+        public string FullName { get; set; }
+
+        /// <summary>
+        /// User's unique email address
+        /// </summary>
+        public string Email { get; set; }
+    }
+}
