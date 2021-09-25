b662228ed6ea0ce8e526eee7a9482af2b5644253
Daniel Klokov <daniel.klokov@mhub.tv>
2021-09-25 20:24:55 +0100

    Registered UserLocationsContext data base context

---
+++ LocationRESTAPI/Models/UserLocationContext.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LocationRESTAPI.Models
+{
+    /// <summary>
+    /// User location data base context
+    /// </summary>
+    public class UserLocationContext : DbContext
+    {
+        /// <summary>
+        /// Data base context constructor with injected options
+        /// </summary>
+        /// <param name="options"></param>
+        public UserLocationContext(DbContextOptions<UserLocationContext> options)
+            : base(options)
+        {
+        }
+
+        /// <summary>
+        /// User entities
+        /// </summary>
+        public DbSet<User> Users { get; set; }
+
+        /// <summary>
+        /// User location entities
+        /// </summary>
+        public DbSet<UserLocation> UserLocations { get; set; }
+    }
+}
